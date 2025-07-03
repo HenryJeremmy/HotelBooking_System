d24d5ceddddc8ee6283e9be89809d81572ea4a43
HenryJeremmy <Henryjeremmy448@gmail.com>
2025-07-03 02:03:30 +0100

    updated

--- src/main/java/com/bolton/booking/SecurityConfig.java
+++ src/main/java/com/bolton/booking/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/logout", "/room/**").permitAll()
+                        .requestMatchers("/", "/login", "/logout", "register", "/room/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
